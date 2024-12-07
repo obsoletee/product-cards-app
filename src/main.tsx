a6d3f16adda047234f475fc3cc0c64e434bac2c7
Iliaz Mamedov <ilzmamedov@gmail.com>
2024-12-08 00:55:42 +0300

    router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
+
 import { router } from './routes';
 
 createRoot(document.getElementById('root')!).render(
--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -1,18 +1,23 @@
+import { lazy } from 'react';
 import {
-  Routes,
   Route,
   createBrowserRouter,
   createRoutesFromElements,
 } from 'react-router';
+
 import { CREATE_PRODUCT, HOME, PRODUCTS } from './routes';
+// import { Home } from '../containers/Home';
+
+const Home = lazy(() => import('../containers/Home/'));
+const ProductsList = lazy(() => import('../containers/ProductsList/'));
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Routes>
-      <Route path={HOME} element={<></>} />
-      <Route path={PRODUCTS} element={<></>} />
+    <>
+      <Route path={HOME} element={<Home />} />
+      <Route path={PRODUCTS} element={<ProductsList></ProductsList>} />
       <Route path={PRODUCTS + '/:id'} element={<></>} />
       <Route path={CREATE_PRODUCT} element={<></>} />
-    </Routes>,
+    </>,
   ),
 );
